4600b8c693d3206eebd322c73438c1ae4c2ce0cb
Ahmed Khelili <71844413+Midoukh@users.noreply.github.com>
2022-01-04 14:08:51 +0100

    Delete .history directory

--- .history/pages/api/addComment_20211227173232.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello from Api");
-// We need this to generate random keys both here and later when we’ll map React Components
--- .history/utils/constants_20220103170705.js
+++
@@ -1,7 +0,0 @@
-export const origin =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000"
-    : "https://admin-blog1.herokuapp.com";
-
-console.log(origin);
-console.log(process.env.NODE_ENV);
